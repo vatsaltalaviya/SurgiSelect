c679131924f97b22ded27eed9ec29e981cf2c7f9
vatsal talaviya <vatsaltalaviyaa@gmail.com>
2025-08-11 12:44:24 +0530

    refactor Footer component: remove unused footer link arrays for improved clarity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,42 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const footerLinks1 = [
-    "About Us",
-    "Join Sales",
-    "Success Stories",
-    "Shipping & Delivery Policy",
-    "Returns & Cancellation Policy",
-    "Press Section",
-    "Advertise with Us",
-    "Investor Section",
-  ];
-  const footerLinks2 = [
-    "Help",
-    "Feedback",
-    "Complaints",
-    "Customer Care",
-    "Jobs & Careers",
-    "Contact Us",
-  ];
-  const suppliersToolKit = [
-    "Sell on SurgiSelect",
-    "Latest BuyLead",
-    "Learning Centre",
-    "Ship With SurgiSelect",
-  ];
-
-  const buyersToolKit = [
-    "Post Your Requirement",
-    "Products You Buy",
-    "Search Products & Suppliers",
-  ];
-
-  const accountingSolutions = [
-    "Accounting Software",
-    "Tally on Mobile",
-    "GST e-Invoice",
-  ];
 
   return (
     <footer className="w-full flex flex-col items-center  md:justify-center">
