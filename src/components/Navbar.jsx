c4758db01da978f52a34534ca890c0334f514b30
vatsal talaviya <vatsaltalaviyaa@gmail.com>
2025-08-19 16:48:50 +0530

    chore: update package.json to add vitest and testing libraries, remove unused dependencies, and update devDependencies
    fix: add missing space in Navbar component
    
    style: format vite.config.js for consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,6 +139,7 @@ const Navbar = ({ logOut }) => {
                           },
                         },
                       }}
+                       
                     />
                   )}
                 />
